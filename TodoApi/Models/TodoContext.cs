d822469de6ef3a39460e6eb1f64d1e1a03577701
umesh <umeshpatra@gmail.com>
2020-02-09 19:38:59 +0530

    Add project files.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
