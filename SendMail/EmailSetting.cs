2bcb73ae71dda3d5023976eb522eea133346ccf5
umesh <umeshpatra@gmail.com>
2020-03-14 15:28:57 +0530

    EmailSample

---
+++ SendMail/EmailSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SendMail
+{
+
+    public class EmailSetting
+    {
+        public string body { get; set; }
+        public string emailFromAddress { get; set; }
+        public string emailToAddress { get; set; }
+        public string enableSSL { get; set; }
+        public string password { get; set; }
+        public string portNumber { get; set; }
+        public string smtpAddress { get; set; }
+        public string subject { get; set; }
+    }
+
+}
